b5384a0e2c1f8b44b81a26de6cbce4c5fce208a0
duckegg-root <1070208184@qq.com>
2026-02-25 11:49:53 +0800

    20260225 2 环境变量调整

--- mod/user.nix
+++ mod/user.nix
@@ -34,15 +34,62 @@
 
   #home-manager
   home-manager.users.duckegg = {pkgs, ... }: {
-    home.packages = with pkgs; [
-      chromium
-    ];
-    
+
+    programs = {
+    # 让 home manager 接管 ~/.profile
+      bash.enable = true;
+
+      zsh = {
+        enable = true;
+        initExtra = builtins.readFile ../resources/.zshrc;
+      };
+
+      neovim = {
+        enable = true;
+	viAlias = true;
+      };
+    };
+
+    home = {
+
+      packages = with pkgs; [
+        chromium
+      ];
+
+      # environment
+      sessionPath = [
+        "$HOME/.local/bin"
+      ];
+
+      file = {
+        # fcitx
+        ".local/share/fcitx5/rime/default.custom.yaml".source = ../resources/fcitx/default.custom.yaml;
+        ".local/share/fcitx5/rime/double_pinyin.schema.yaml".source = ../resources/fcitx/double_pinyin.schema.yaml;
+        ".local/share/fcitx5/themes" = {
+          source = ../resources/fcitx/themes;
+          recursive = true;
+        };
+
+        # kitty
+        ".config/kitty/kitty.conf".source = ../resources/kitty.conf;
+
+        # font
+        ".local/share/fonts".source = ../resources/font;
+
+        # fuzzel theme
+        ".config/fuzzel".source = ../resources/fuzzel;
+ 
+        #cursor
+        ".local/share/icons".source = ../resources/cursors;
+
+	#nvi -> nvim
+	".local/bin/nvi".source = "${pkgs.neovim}/bin/nvim";
+      };
+    };
+
     # niri config
     xdg.configFile."niri/config.kdl".text = ''
       include "/etc/nixos/resources/niri-config.kdl"
-
-      // spawn-at-startup "waybar"
 
       prefer-no-csd
 
@@ -53,26 +100,5 @@
     
     home.stateVersion = "25.11";
 
-    home.file = {
-      # fcitx
-      ".local/share/fcitx5/rime/default.custom.yaml".source = ../resources/fcitx/default.custom.yaml;
-      ".local/share/fcitx5/rime/double_pinyin.schema.yaml".source = ../resources/fcitx/double_pinyin.schema.yaml;
-      ".local/share/fcitx5/themes" = {
-        source = ../resources/fcitx/themes;
-	recursive = true;
-      };
-
-      # kitty
-      ".config/kitty/kitty.conf".source = ../resources/kitty.conf;
-
-      # font
-      ".local/share/fonts".source = ../resources/font;
-
-      # fuzzel theme
-      ".config/fuzzel".source = ../resources/fuzzel;
-
-      #cursor
-      ".local/share/icons".source = ../resources/cursors;
-    };
   };
 }
